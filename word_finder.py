eee0fbc43a27cd2561ec1e1abfb7ee54ade32fcf
Connor <connorbarkman@icloud.com>
2021-03-30 18:28:40 -0400

    added functionality word appearing more than once

--- word_finder.py
+++ word_finder.py
@@ -1,5 +1,6 @@
 import time
 import dictionaryload
+import re
 
 word_list = dictionaryload.load('2of4brif.txt')
 
@@ -7,10 +8,16 @@ start_time = time.time()
 def word_finder(string):
     # finds all words within a string
     words = {}
+    # print(string)
     string.replace(" ", "")
     for i in word_list:
         if i in string:
-            words[i]= (string.find(i), string.find(i)+len(i)-1)
+            count= 0
+            for w in re.finditer(i, string):
+
+                # words[i]= (string.find(i), string.find(i)+len(i)-1)
+                words[i+str(count)] = (w.start(), w.end()-1)
+                count+=1
     
     return words
 
@@ -22,24 +29,30 @@ def get_key(val, dict):
 
 
 
-def pathfinder(startig, start, length):
-    # gets the correct word and position of each word in the sentence 
+def word_joiner(word_dict, string):
+    # finds a start point and joins together the positions 
+    starts = sorted(list(word_dict.values()), key = lambda x: x[0]) 
+    # print("string",string)
+
     pos_final = []
-    
+    length = len(string)
+    startig = starts[0]
+    start = starts
+    future = 0
     def recurse(starting, starts):
         next_pos = [i for i in starts if i[0] == starting[1]+1]
         print("recurse")
         if len(starts) == 0:
-            print("none")
-            return None
+            
+            return ""
         if len(next_pos) == 0:
             if starting[1] + 1 == length:
                 print("final")
-                start = starts
-                return pos_final
+                
+                return #[start[0]] + pos_final
 
             else:
-                print(str(starting) + " from " + str(starts))
+                print(str((get_key(starting, word_dict)+ str(starting))) + " from " + str(dict(zip([get_key(i, word_dict) for i in starts], starts))))
                 starts.remove(starting)
                 startig = starts[0]
                 pos_final.clear()
@@ -50,19 +63,18 @@ def pathfinder(startig, start, length):
             nexts = next_pos[0]
             pos_final.append(nexts)
             recurse(nexts, starts)
-    recurse(startig, start)
-    return [start[0]] + pos_final
-
-
-def word_joiner(word_dict, string):
-    # finds a start point and joins together the positions 
-    starts = sorted(list(word_dict.values()), key = lambda x: x[0]) 
+        # try:
+        #     recurse(startig, start)
+        # except IndexError:
+        #     return ""
     
-    words = pathfinder(starts[0], starts, len(string))
+    
+    words = recurse(startig, start)
+    sentence = [start[0]] + pos_final
+    print(words)
+    
+    return " ".join([get_key(i, word_dict) for i in sentence])
 
-    return " ".join([get_key(i, word_dict) for i in words])
-
-        
-print(word_joiner(word_finder("inawanttoeatfood"), "inawanttoeatfood"))
+print(word_finder("bobwantstogotogreatadventuretomorrowbutiwouldrathergototheshoreifwegototheamusementparkwewillspendalldayinline"), "bobwantstogotogreatadventuretomorrowbutiwouldrathergototheshoreifwegototheamusementparkwewillspendalldayinline")
+# print(word_joiner(word_finder("bobwantstogotogreatadventuretomorrowbutiwouldrathergototheshoreifwegototheamusementparkwewillspenalldayinline"), "bobwantstogotogreatadventuretomorrowbutiwouldrathergototheshoreifwegototheamusementparkwewillspenalldayinline"))
 print("--- %s seconds ---" % (time.time() - start_time))
-
