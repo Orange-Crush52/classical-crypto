50d2f971cf33f86a0acd48fd8bddf529332c7f69
Connor <connorbarkman@icloud.com>
2021-03-30 23:28:51 -0400

    word finding works only problem is partial words

--- word_finding_tree_path.py
+++ word_finding_tree_path.py
@@ -1,9 +1,9 @@
 from typing import List
 import word_finder
-
+import time 
 word_dict = word_finder.word_finder("bobwantstogotogreatadventuretomorrowbutiwouldrathergototheshoreifwegototheamusementparkwewillspendalldayinline")
 
-starts = sorted(list(word_dict.values()), key = lambda x: x[0])
+starts = sorted(list(word_dict.values()), key = lambda x: x[1])
 # print(starts)
 
 next_pos = lambda lengths, point : [length for length in lengths if length[0] == point[1]+1]
@@ -56,10 +56,26 @@ class Tree:
         # branch is the begining of the list being removed
         # branches is all of the tree objects
         more = True
+        # print("cut")
+        # print(lsts)
         while more:
             # print("more", more)
             # print(branch)
+            # for i in lsts:
+            #     # print("branch", branch)
+            #     # print([get_key(j, word_dict) for j in [i.data for i in lsts[i]]])
+            #     if branch in lsts[i]:
+            #         # print("!!!!!!!!!!!!!!!!!!!!!!!!")
+            #         # print(branch)
+            #         # print(lsts[i])
+            #         # print(branch in lsts[i])
+            #         # print("!!!!!!!!!!!!!!!!!!!!!!")
+            #         lsts[i] = [i for i in lsts[i] if i != branch]
             del branchess[get_key(branch, branchess)]
+            parents = self.parent(branch, branchess)
+                
+            for parent in parents:
+                branchess[parent].children.remove(branch)
             if len(branch.children) > 0:
                 branch = branch.children[0]
                 # print(branch.data)
@@ -67,16 +83,19 @@ class Tree:
             else:
                 # print("else")
                 # can occasionaly have more than one parent
-                parents = self.parent(branch, branchess)
+                # parents = self.parent(branch, branchess)
                 
-                for parent in parents:
-                    branchess[parent].children.remove(branch)
+                # for parent in parents:
+                #     branchess[parent].children.remove(branch)
                 more = False
                 
         return branchess
 
-
-
+# two fixes sort by length reversed
+# find out whats actualy going wrong 
+# I think its that when the branches are deleted tha values aren't deleted off of straight and split
+# should problaby do both
+# should never be anything ahead of the current word 
 def tree_grower(searche, length):
     global branches, split, straight, pos_finals
 
@@ -92,27 +111,33 @@ def tree_grower(searche, length):
     straight = Inter([])
     root = list(branches.keys())[0]
     def tree_trimmer(current):
-    
+        # get_key(j, word_dict) for j in 
         global branches, split, straight, pos_finals
-        # try:
-        # print("split", [i.data for i in split.items] , "straight", [i.data for i in straight.items], "current", current.data)
-        # except AttributeError:
-        #     print("split", [i.data for i in split.items] , "current", current.data, "straight", straight.items)
+
+
+        try:
+            print("split", [get_key(j, word_dict) for j in [i.data for i in split.items]] , "straight",[get_key(j, word_dict) for j in [i.data for i in straight.items]], "current", get_key(current.data, word_dict))
+            print(get_key(current.data, word_dict), [get_key(i, word_dict) for i in [j.data for j in current.children]])
+        except AttributeError:
+            print("split", [get_key(j, word_dict) for j in [i.data for i in split.items]] , "current", get_key(current.data, word_dict), "straight", straight.items)
         if len(current.children) >1:
             # print(current in branches, current.data, current)
             # print(str(current.data) + " is a split")
             split.add(current)
             
             # print("more than one")
+            
+            # print([i.data for i in sorted(current.children,key=lambda x: len(get_key(x.data, word_dict)),reverse=True)])
+            # tree_trimmer(sorted(current.children,key=lambda x: len(get_key(x.data, word_dict)),reverse=True)[0])
             tree_trimmer(current.children[0])
-        
         elif len(current.children) == 1:
             # print(str(current.data) + " is a straight")
             # print("one")
+            # current.children[0]
             straight.add(current)
-
+            # print([i.data for i in sorted(current.children,key=lambda x: len(get_key(x.data, word_dict)),reverse=True)])
+            # tree_trimmer(sorted(current.children,key=lambda x: len(get_key(x.data, word_dict)),reverse=True)[0])
             tree_trimmer(current.children[0])
-        
 
         else:
             # print(current in branches, current.data, current)
@@ -134,24 +159,39 @@ def tree_grower(searche, length):
                     # print("try")
                     # print("cut",cut(straight).data)
                     # branches = cut(straight).branch_cutter(cut(straight), branches)
+                    # print("!!!!!!!!!!!!!!!!!!!!!!!", straight.items)
+                    # print("straight items", [i for i in straight.items if i.data[0]>0])
+                    # print(cut(straight).data[1])
+                    # print(split.items[-1].data)
+                    print("adf", [i for i in straight.items if i.data[0]>0])
+                    print(type(cut(split).data[1]))
+                    far = [i for i in straight.items if i.data[0] >= cut(split).data[1]]
+                    print("start !!!!!!!!!!!!!!!!!!!!!!!", far) 
+                    for i in far:
+                        straight.items.remove(i)
                     branches = current.branch_cutter(current, branches)
-                    # print("try success")
+                    # branches = start.branch_cutter(start, branches)
+                    print("try success huzahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
                 except (IndexError, AttributeError):
-                    # print("except")
+                    print("except")
                     
                     branches = current.branch_cutter(current, branches)
-                    
+                #########################
+                # if this is last split then you should delete the first straight piece after
                 tree_trimmer(cut(split))
     
     tree_trimmer(branches[root])
     
     return " ".join([get_key(i, word_dict)[:-1] for i in pos_finals])
 
-print(word_dict)
+start_time = time.time()
+# print(word_dict)
 print(tree_grower(starts, 110))
-
+print(word_dict)
 # print(len("bobwantstogotogreatadventuretomorrowbutiwouldrathergototheshoreifwegototheamusementparkwewillspendalldayinline"), "bobwantstogotogreatadventuretomorrowbutiwouldrathergototheshoreifwegototheamusementparkwewillspendalldayinline")
 
+#lsts isn't doing anything its just a record
 
+print("--- %s seconds ---" % (time.time() - start_time))
 
 
